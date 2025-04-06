ed9789426cec9f8f50c263b997e89837ef1c19ae
Yaroslav <fishatary@proton.me>
2025-04-06 20:55:40 +0300

    fix: change default theme to light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={`${rubik.className} antialiased`}>
         <ThemeProvider
           attribute='class'
-          defaultTheme='system'
+          defaultTheme='light'
           enableSystem
           disableTransitionOnChange
         >
